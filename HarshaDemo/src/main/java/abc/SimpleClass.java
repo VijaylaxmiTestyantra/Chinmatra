8541fc2965ef525691679c44543d35914f0ccd28
VijaylaxmiTestyantra <120176381+VijaylaxmiTestyantra@users.noreply.github.com>
2024-11-30 13:26:05 +0530

    Update SimpleClass.java

--- HarshaDemo/src/main/java/abc/SimpleClass.java
+++ HarshaDemo/src/main/java/abc/SimpleClass.java
@@ -5,6 +5,8 @@ public class SimpleClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("I am Vijaylaxmi");
+System.out.println("I am Vijaylaxmi Halkarni");
+
 	}
 
 }
